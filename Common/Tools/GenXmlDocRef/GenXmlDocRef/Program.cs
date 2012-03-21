5cecbab2899a36b288f53c39835e839fec039a5b
Zihan Chen <vczh@163.com>
2012-03-21 20:30:43 +0000

    GenXmlDocRef

---
+++ Common/Tools/GenXmlDocRef/GenXmlDocRef/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace GenXmlDocRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                XDocument docXml = XDocument.Load(args[0]);
+                string outputFolder = Path.GetFullPath(args[1]) + "\\";
+
+                var docRoot = DocItemAnalyzer.GetItems(docXml.Root);
+                Dictionary<string, DocItem> uniqueIdItemMap = new Dictionary<string, DocItem>();
+                docRoot.BuildIndex(uniqueIdItemMap);
+            }
+            else
+            {
+                Console.WriteLine("GenXmlDocRef.exe <doc-xml> <out-folder>");
+            }
+        }
+    }
+}
