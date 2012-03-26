a9bebb9e1374fb8ca1d3e746df83713d3e9fbaa1
vczh_cp <SND\vczh_cp>
2012-03-26 06:04:56 +0000

    Add GenXmlDocRef.exe to Tools folder

--- Libraries/GacUI/Public/Release.bat
+++ Libraries/GacUI/Public/Release.bat
@@ -6,4 +6,5 @@ copy ..\..\GacUISrc\Debug\GacUISrc.xml GacUISrc.xml
 copy ..\..\GacUISrc\Debug\GacUISrc.pdb GacUISrc.pdb
 ..\..\..\..\Common\Tools\PdbDump.exe GacUISrc.pdb GacUISrcPdb.xml
 ..\..\..\..\Common\Tools\LinkPdbXml.exe GacUISrcPdb.xml GacUISrc.xml GacUISrcDoc.xml
+..\..\..\..\Common\Tools\GenXmlDocRef.exe GacUISrcDoc.xml . reference:gacui
 pause
