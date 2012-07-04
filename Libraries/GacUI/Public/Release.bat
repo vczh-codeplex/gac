b6f1eadc551843f8194e5d39fa2c02f3b731edb6
vczh_cp <SND\vczh_cp>
2012-07-04 15:58:52 +0000

    StaticMetaDoc generation

--- Libraries/GacUI/Public/Release.bat
+++ Libraries/GacUI/Public/Release.bat
@@ -12,13 +12,13 @@ copy ..\..\GacUISrc\Debug\GacUISrc.pdb GacUISrc.pdb
 ..\..\..\..\Common\Tools\PdbDump.exe GacUISrc.pdb GacUISrcPdb.xml
 ..\..\..\..\Common\Tools\LinkPdbXml.exe GacUISrcPdb.xml GacUISrc.xml GacUISrcDoc.xml
 ..\..\..\..\Common\Tools\GenXmlDocRef.exe GacUISrcDoc.xml . reference:gacui
+mkdir StaticMetaDoc
 cd ..
+..\..\..\Common\Tools\GenDocIndex.exe meta Temp\StaticMetaDoc Temp DocumentRawItems
 ..\..\..\Common\Tools\GenDocIndex.exe html Web\StaticHtmlDoc Temp DocumentRawItems
-echo MAKING RELEASE FOLDER...
 ..\..\..\Common\Tools\Recurcpy.exe Source				Temp\ReleaseFolder\Public\Source  include cpp h
 ..\..\..\Common\Tools\Recurcpy.exe Web\StaticHtmlDoc	Temp\ReleaseFolder\Public\Doc     include html
 ..\..\..\Common\Tools\Recurcpy.exe ..\GacUIDemo			Temp\ReleaseFolder\GacUIDemo      include sln vssscc vcxproj filters vspscc cpp h bmp jpg gif png ico
 copy Web\PageStyle.css									Temp\ReleaseFolder\Public\PageStyle.css
 copy Web\DocStyle.css									Temp\ReleaseFolder\Public\DocStyle.css
-echo RELEASE FOLDER: %CD%\Temp\ReleaseFolder\
 pause
