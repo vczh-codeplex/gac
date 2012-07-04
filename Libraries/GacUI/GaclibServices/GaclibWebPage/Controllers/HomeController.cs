6b86e83a8f937296fd8ec4c48b6939acd3719cc2
vczh_cp <SND\vczh_cp>
2012-07-04 09:03:12 +0000

    Demos page with model

--- Libraries/GacUI/GaclibServices/GaclibWebPage/Controllers/HomeController.cs
+++ Libraries/GacUI/GaclibServices/GaclibWebPage/Controllers/HomeController.cs
@@ -9,6 +9,27 @@ namespace GaclibWebPage.Controllers
 {
     public class HomeController : Controller
     {
+        private static string[] demoControls = new string[]
+        {
+            "Button.CheckAndRadio",
+            "Button.EnableDisable",
+            "Label.Hyperlink",
+
+            "Tab.TextBoxPage",
+            "TextBox.Editor",
+            "TextBox.Colorizer",
+
+            "ListBox.NameEditor",
+            "ListBox.NameSelector",
+            "ListBox.VirtualMode",
+
+            "ListView.ViewSwitching",
+            "ListView.VirtualMode",
+            "ListView.SortingAndFiltering",
+
+            "TreeView.FileExplorer",
+        };
+
         public ActionResult Index(NavigationBarModel model)
         {
             model.ActiveButton = NavigationButtons.Home;
@@ -21,9 +42,17 @@ namespace GaclibWebPage.Controllers
             return View(model);
         }
 
-        public ActionResult Demos(NavigationBarModel model)
+        public ActionResult Demos(DemoListModel model)
         {
             model.ActiveButton = NavigationButtons.Demos;
+            model.DemoDescriptions =
+                demoControls
+                .Select(name => new DemoDescription
+                {
+                    Name = "Controls." + name,
+                    Title = name,
+                })
+                .ToArray();
             return View(model);
         }
 
---
+++ Libraries/GacUI/GaclibServices/GaclibWebPage/Models/DemoListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GaclibWebPage.Models
+{
+    public class DemoDescription
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class DemoListModel : NavigationBarModel
+    {
+        public DemoDescription[] DemoDescriptions { get; set; }
+
+        public DemoListModel()
+        {
+            this.DemoDescriptions = new DemoDescription[0];
+        }
+    }
+}
