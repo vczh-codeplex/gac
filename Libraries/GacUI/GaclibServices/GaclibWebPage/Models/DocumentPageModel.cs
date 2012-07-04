0ff72c5b44f932a8cfcfc1af106bd27cd79e64ba
Zihan Chen <vczh@163.com>
2012-07-04 18:08:07 +0000

    Document page skeleton

---
+++ Libraries/GacUI/GaclibServices/GaclibWebPage/Models/DocumentPageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GaclibWebPage.Models
+{
+    public class DocumentPageModel
+    {
+        public string Title { get; set; }
+    }
+}
