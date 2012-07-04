07570d4f851d5feb9eb69ba0bfa32163f0dc94e4
Zihan Chen <vczh@163.com>
2012-07-04 08:13:49 +0000

    Gaclib webpage navigation bar

--- Libraries/GacUI/GaclibServices/GaclibWebPage/Controllers/HomeController.cs
+++ Libraries/GacUI/GaclibServices/GaclibWebPage/Controllers/HomeController.cs
@@ -14,5 +14,29 @@ namespace GaclibWebPage.Controllers
             model.ActiveButton = NavigationButtons.Home;
             return View(model);
         }
+
+        public ActionResult GettingStart(NavigationBarModel model)
+        {
+            model.ActiveButton = NavigationButtons.GettingStart;
+            return View(model);
+        }
+
+        public ActionResult Demos(NavigationBarModel model)
+        {
+            model.ActiveButton = NavigationButtons.Demos;
+            return View(model);
+        }
+
+        public ActionResult Download(NavigationBarModel model)
+        {
+            model.ActiveButton = NavigationButtons.Download;
+            return View(model);
+        }
+
+        public ActionResult Contact(NavigationBarModel model)
+        {
+            model.ActiveButton = NavigationButtons.Contact;
+            return View(model);
+        }
     }
 }
