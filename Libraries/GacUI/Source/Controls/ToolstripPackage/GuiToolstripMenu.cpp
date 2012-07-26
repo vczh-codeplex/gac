c98da0986a28b8543660864c40cbd9404734c55c
Zihan Chen <vczh@163.com>
2012-07-26 12:15:33 +0000

    GuiToolstripButton

--- Libraries/GacUI/Source/Controls/ToolstripPackage/GuiToolstripMenu.cpp
+++ Libraries/GacUI/Source/Controls/ToolstripPackage/GuiToolstripMenu.cpp
@@ -6,6 +6,78 @@ namespace vl
 	{
 		namespace controls
 		{
+
+/***********************************************************************
+GuiToolstripButton
+***********************************************************************/
+
+			void GuiToolstripButton::UpdateCommandContent()
+			{
+				if(command)
+				{
+					SetImage(command->GetImage());
+					SetText(command->GetText());
+					SetEnabled(command->GetEnabled());
+					if(command->GetShortcut())
+					{
+						SetShortcutText(command->GetShortcut()->GetName());
+					}
+					else
+					{
+						SetShortcutText(L"");
+					}
+				}
+				else
+				{
+					SetImage(0);
+					SetText(L"");
+					SetEnabled(true);
+					SetShortcutText(L"");
+				}
+			}
+
+			void GuiToolstripButton::OnClicked(compositions::GuiGraphicsComposition* sender, compositions::GuiEventArgs& arguments)
+			{
+				if(command)
+				{
+					command->Executed.ExecuteWithNewSender(arguments, sender);
+				}
+			}
+
+			void GuiToolstripButton::OnCommandDescriptionChanged(compositions::GuiGraphicsComposition* sender, compositions::GuiEventArgs& arguments)
+			{
+				UpdateCommandContent();
+			}
+
+			GuiToolstripButton::GuiToolstripButton(IStyleController* _styleController)
+				:GuiMenuButton(_styleController)
+			{
+			}
+
+			GuiToolstripButton::~GuiToolstripButton()
+			{
+			}
+
+			Ptr<GuiToolstripCommand> GuiToolstripButton::GetCommand()
+			{
+				return command;
+			}
+
+			void GuiToolstripButton::SetCommand(Ptr<GuiToolstripCommand> value)
+			{
+				if(command)
+				{
+					command->DescriptionChanged.Detach(descriptionChangedHandler);
+				}
+				command=0;
+				descriptionChangedHandler=0;
+				if(value)
+				{
+					command=value;
+					descriptionChangedHandler=command->DescriptionChanged.AttachMethod(this, &GuiToolstripButton::OnCommandDescriptionChanged);
+				}
+				UpdateCommandContent();
+			}
 		}
 	}
 }
