6c8419595b7698f6fc656cb64bc9a3c37252656d
vczh_cp <SND\vczh_cp>
2012-07-26 12:15:33 +0000

    GuiToolstripButton

--- Libraries/GacUI/Source/Controls/ToolstripPackage/GuiToolstripMenu.h
+++ Libraries/GacUI/Source/Controls/ToolstripPackage/GuiToolstripMenu.h
@@ -18,6 +18,29 @@ namespace vl
 	{
 		namespace controls
 		{
+			/// <summary>Toolstrip button that can connect with a <see cref="GuiToolstripCommand"/>.</summary>
+			class GuiToolstripButton : public GuiMenuButton, public Description<GuiToolstripButton>
+			{
+			protected:
+				Ptr<GuiToolstripCommand>						command;
+				Ptr<compositions::GuiNotifyEvent::IHandler>		descriptionChangedHandler;
+
+				void											UpdateCommandContent();
+				void											OnClicked(compositions::GuiGraphicsComposition* sender, compositions::GuiEventArgs& arguments);
+				void											OnCommandDescriptionChanged(compositions::GuiGraphicsComposition* sender, compositions::GuiEventArgs& arguments);
+			public:
+				/// <summary>Create a control with a specified style controller.</summary>
+				/// <param name="_styleController">The style controller.</param>
+				GuiToolstripButton(IStyleController* _styleController);
+				~GuiToolstripButton();
+
+				/// <summary>Get the attached <see cref="GuiToolstripCommand"/>.</summary>
+				/// <returns>The attached toolstrip command.</returns>
+				Ptr<GuiToolstripCommand>						GetCommand();
+				/// <summary>Detach from the previous <see cref="GuiToolstripCommand"/> and attach to a new one. If the command is null, this function only do detaching.</summary>
+				/// <param name="value">The new toolstrip command.</param>
+				void											SetCommand(Ptr<GuiToolstripCommand> value);
+			};
 		}
 	}
 }
