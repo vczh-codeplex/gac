c4ed30707ea6845980477c228a318b405d13c57a
vczh_cp <SND\vczh_cp>
2012-07-28 18:00:33 +0000

    Toolstrip container automatically call sub componet measurer

--- Libraries/GacUI/Source/GraphicsComposition/GuiGraphicsComposition.cpp
+++ Libraries/GacUI/Source/GraphicsComposition/GuiGraphicsComposition.cpp
@@ -147,7 +147,7 @@ GuiSubComponentMeasurer
 						WString name=source->GetSubComponentName(i);
 						GuiGraphicsComposition* composition=source->GetSubComponentComposition(i);
 						composition->SetPreferredMinSize(Size(0, 0));
-						Size size=composition->GetBounds().GetSize();
+						Size size=composition->GetPreferredBounds().GetSize();
 						int sizeComponent=direction==Horizontal?size.x:size.y;
 
 						int index=sizes.Keys().IndexOf(name);
