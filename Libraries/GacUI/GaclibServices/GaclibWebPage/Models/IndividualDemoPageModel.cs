0adb9003453293f84c04b577bc9ab903576a03a2
Zihan Chen <vczh@163.com>
2012-09-24 06:36:11 +0000

    Update the website

--- Libraries/GacUI/GaclibServices/GaclibWebPage/Models/IndividualDemoPageModel.cs
+++ Libraries/GacUI/GaclibServices/GaclibWebPage/Models/IndividualDemoPageModel.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace GaclibWebPage.Models
 {
-    public class IndividualDemoPageModel : NavigationBarModel
+    public class IndividualDemoModel : NavigationBarModel
     {
         public string Title { get; set; }
         public string Name { get; set; }
@@ -14,11 +14,18 @@ namespace GaclibWebPage.Models
         public string Description { get; set; }
         public Dictionary<string, string> CppCodes { get; set; }
 
-        public IndividualDemoPageModel()
+        public IndividualDemoModel()
         {
             this.Title = "";
             this.Images = new string[0];
             this.CppCodes = new Dictionary<string, string>();
         }
+    }
+
+    public class IndividualDemoCategoryModel : NavigationBarModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IndividualDemoModel[] Demos { get; set; }
     }
 }
