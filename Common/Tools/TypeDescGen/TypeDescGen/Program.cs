c0b2b2b44ad0c867ddac768981e894d3f04ff531
vczh_cp <SND\vczh_cp>
2012-10-08 05:08:57 +0000

    Update compilation command line tools to .NET 4.5

---
+++ Common/Tools/TypeDescGen/TypeDescGen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeDescGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("TypeDescGen.exe <config-xml>");
+                return;
+            }
+        }
+    }
+}
