7f71c2ff7b05c366616244240a774bbcc98d892f
Zihan Chen <vczh@163.com>
2012-11-16 06:47:10 +0000

    Vczh Services Deployment

---
+++ Common/ExperimentalTests/SimpleHttpServer/DeployServer/DeployServerForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace DeployServer
+{
+    partial class DeployServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DeployServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 558);
+            this.Name = "DeployServerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vczh Services Deployment Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
