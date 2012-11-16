05ec23076f63804733013ffa34a93c2c5fbc300b
vczh_cp <SND\vczh_cp>
2012-11-16 17:39:48 +0000

    Try to build spider and search api for niaowo.me

---
+++ Common/ExperimentalTests/SimpleHttpServer/NestleServices/NestleSearch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SimpleHttpServer;
+
+namespace NestleSearch
+{
+    class Program : IDeployServerCallback
+    {
+        [Get("")]
+        public string Default()
+        {
+            return "Welcome to Nestle Search!";
+        }
+
+        public void Stop()
+        {
+            stopEvent.Set();
+        }
+
+        static ManualResetEvent stopEvent = null;
+
+        static void Main(string[] args)
+        {
+            if (SimpleHttpServerHost.StartService<Program>("NestleSearch"))
+            {
+                stopEvent = new ManualResetEvent(false);
+                stopEvent.WaitOne();
+            }
+            else
+            {
+                Console.WriteLine("Press [ENTER] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
