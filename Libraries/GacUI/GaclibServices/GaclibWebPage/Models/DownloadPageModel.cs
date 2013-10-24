84d06fd8a5e00e4c219b4f7ce43b1a66b4732cd1
vczh_cp <SND\vczh_cp>
2013-10-24 06:21:31 +0000

    Update Website

--- Libraries/GacUI/GaclibServices/GaclibWebPage/Models/DownloadPageModel.cs
+++ Libraries/GacUI/GaclibServices/GaclibWebPage/Models/DownloadPageModel.cs
@@ -12,6 +12,7 @@ namespace GaclibWebPage.Models
         public DownloadPageModel()
         {
             this.Versions = new DownloadVersionUrls[] { 
+                new DownloadVersionUrls{ Version="0.5.5.0", Url="https://gac.codeplex.com/releases/view/113889" },
                 new DownloadVersionUrls{ Version="0.5.4.0", Url="https://gac.codeplex.com/releases/view/112090" },
                 new DownloadVersionUrls{ Version="0.5.3.0", Url="https://gac.codeplex.com/releases/view/110703" },
                 new DownloadVersionUrls{ Version="0.5.2.0", Url="https://gac.codeplex.com/releases/view/108424" },
