2e0426fd28f9080034b513443db660f0ff620669
Zihan Chen <vczh@163.com>
2013-11-20 08:37:44 +0000

    GacStudio

---
+++ Libraries/GacUI/GacUISrc/GacStudio/Main.cpp
@@ -0,0 +1,21 @@
+#if _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include "..\..\Public\Source\GacUI.h"
+#include "..\..\Public\Source\GacUIReflection.h"
+
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int CmdShow)
+{
+	return SetupWindowsDirect2DRenderer();
+}
+
+void GuiMain()
+{
+}
