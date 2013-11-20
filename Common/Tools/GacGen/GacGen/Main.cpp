4cb8e3fecb30c424acde531f233c17d6e6e9c815
Zihan Chen <vczh@163.com>
2013-11-20 08:46:49 +0000

    GacGen

---
+++ Common/Tools/GacGen/GacGen/Main.cpp
@@ -0,0 +1,13 @@
+#include "..\..\..\..\Libraries\GacUI\Public\Source\GacUIReflection.h"
+
+using namespace vl::console;
+
+int main()
+{
+	SetupWindowsDirect2DRenderer();
+}
+
+void GuiMain()
+{
+	Console::WriteLine(L"Welcome to GacUI Resource Code Behind Generator");
+}
