dcd94accd3e6f9a50d80966161a93732f9721d36
vczh_cp <SND\vczh_cp>
2014-06-24 07:25:58 +0000

    GuiSharedSize(Root|Item)Composition

--- Libraries/GacUI/Source/GraphicsComposition/GuiGraphicsComposition.cpp
+++ Libraries/GacUI/Source/GraphicsComposition/GuiGraphicsComposition.cpp
@@ -146,6 +146,165 @@ GuiSubComponentMeasurer
 					source->GetContainerComposition()->ForceCalculateSizeImmediately();
 				}
 			}
+
+/***********************************************************************
+GuiSharedSizeUnitComposition
+***********************************************************************/
+
+			void GuiSharedSizeItemComposition::Update()
+			{
+				if (parentRoot)
+				{
+					parentRoot->ForceCalculateSizeImmediately();
+				}
+			}
+
+			void GuiSharedSizeItemComposition::OnParentLineChanged()
+			{
+				GuiBoundsComposition::OnParentLineChanged();
+				if (parentRoot)
+				{
+					parentRoot->childItems.Remove(this);
+					parentRoot = 0;
+				}
+
+				auto current = GetParent();
+				while (current)
+				{
+					if (auto item = dynamic_cast<GuiSharedSizeItemComposition*>(current))
+					{
+						break;
+					}
+					else if (auto root = dynamic_cast<GuiSharedSizeRootComposition*>(current))
+					{
+						parentRoot = root;
+						break;
+					}
+					current = current->GetParent();
+				}
+
+				if (parentRoot)
+				{
+					parentRoot->childItems.Add(this);
+				}
+			}
+
+			GuiSharedSizeItemComposition::GuiSharedSizeItemComposition()
+				:parentRoot(0)
+				, sharedWidth(false)
+				, sharedHeight(false)
+			{
+			}
+
+			GuiSharedSizeItemComposition::~GuiSharedSizeItemComposition()
+			{
+			}
+
+			const WString& GuiSharedSizeItemComposition::GetGroup()
+			{
+				return group;
+			}
+
+			void GuiSharedSizeItemComposition::SetGroup(const WString& value)
+			{
+				if (group != value)
+				{
+					group = value;
+					Update();
+				}
+			}
+
+			bool GuiSharedSizeItemComposition::GetSharedWidth()
+			{
+				return sharedWidth;
+			}
+
+			void GuiSharedSizeItemComposition::SetSharedWidth(bool value)
+			{
+				if (sharedWidth != value)
+				{
+					sharedWidth = value;
+					Update();
+				}
+			}
+
+			bool GuiSharedSizeItemComposition::GetSharedHeight()
+			{
+				return sharedHeight;
+			}
+
+			void GuiSharedSizeItemComposition::SetSharedHeight(bool value)
+			{
+				if (sharedHeight != value)
+				{
+					sharedHeight = value;
+					Update();
+				}
+			}
+
+/***********************************************************************
+GuiSharedSizeRootComposition
+***********************************************************************/
+
+			GuiSharedSizeRootComposition::GuiSharedSizeRootComposition()
+			{
+			}
+
+			GuiSharedSizeRootComposition::~GuiSharedSizeRootComposition()
+			{
+			}
+
+			void AddSizeComponent(Dictionary<WString, vint>& sizes, const WString& group, vint sizeComponent)
+			{
+				vint index = sizes.Keys().IndexOf(group);
+				if (index == -1)
+				{
+					sizes.Add(group, sizeComponent);
+				}
+				else if (sizes.Values().Get(index) < sizeComponent)
+				{
+					sizes.Set(group, sizeComponent);
+				}
+			}
+
+			void GuiSharedSizeRootComposition::ForceCalculateSizeImmediately()
+			{
+				Dictionary<WString, vint> widths, heights;
+
+				FOREACH(GuiSharedSizeItemComposition*, item, childItems)
+				{
+					auto group = item->GetGroup();
+					auto size = item->GetPreferredBounds().GetSize();
+
+					if (item->GetSharedWidth())
+					{
+						AddSizeComponent(widths, group, size.x);
+					}
+					if (item->GetSharedHeight())
+					{
+						AddSizeComponent(widths, group, size.y);
+					}
+				}
+
+				FOREACH(GuiSharedSizeItemComposition*, item, childItems)
+				{
+					auto group = item->GetGroup();
+					auto size = item->GetPreferredMinSize();
+
+					if (item->GetSharedWidth())
+					{
+						size.x = widths[group];
+					}
+					if (item->GetSharedHeight())
+					{
+						size.y = heights[group];
+					}
+
+					item->SetPreferredMinSize(size);
+				}
+
+				GuiBoundsComposition::ForceCalculateSizeImmediately();
+			}
 		}
 	}
 }
