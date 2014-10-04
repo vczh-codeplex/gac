5f32f0f87b2d98bcadd106969eb690a572e911a2
vczh_cp <SND\vczh_cp>
2014-10-04 07:58:08 +0000

    Enable Windows native message handling extension for Windows OS

--- Libraries/GacUI/Source/NativeWindow/Windows/WinNativeWindow.h
+++ Libraries/GacUI/Source/NativeWindow/Windows/WinNativeWindow.h
@@ -23,12 +23,21 @@ namespace vl
 Windows Platform Native Controller
 ***********************************************************************/
 
+			class INativeMessageHandler : public Interface
+			{
+			public:
+				virtual void								BeforeHandle(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam, bool& skip) = 0;
+				virtual void								AfterHandle(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam, bool& skip, LRESULT& result) = 0;
+			};
+
 			class IWindowsForm : public Interface
 			{
 			public:
-				virtual HWND								GetWindowHandle()=0;
-				virtual Interface*							GetGraphicsHandler()=0;
-				virtual void								SetGraphicsHandler(Interface* handler)=0;
+				virtual HWND								GetWindowHandle() = 0;
+				virtual Interface*							GetGraphicsHandler() = 0;
+				virtual void								SetGraphicsHandler(Interface* handler) = 0;
+				virtual bool								InstallMessageHandler(Ptr<INativeMessageHandler> handler) = 0;
+				virtual bool								UninstallMessageHandler(Ptr<INativeMessageHandler> handler) = 0;
 			};
 
 			extern INativeController*						CreateWindowsNativeController(HINSTANCE hInstance);
