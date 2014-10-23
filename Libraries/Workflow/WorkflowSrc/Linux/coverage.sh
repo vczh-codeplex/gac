04f3d78c1352421f0c191920d0c391cf4f47b89e
Zihan Chen <vczh@163.com>
2014-10-23 14:41:34 +0000

    gcov

--- Libraries/Workflow/WorkflowSrc/Linux/coverage.sh
+++ Libraries/Workflow/WorkflowSrc/Linux/coverage.sh
@@ -1,3 +1,6 @@
 mkdir Coverage
-gcov -o ./Obj/ ../../Source/*.cpp ../../Source/Expression/*.cpp ../../Source/Analyzer/*.cpp ../../Source/Runtime/*.cpp
+gcov -o ./Obj/ -l ../../Source/*.cpp
+gcov -o ./Obj/ -l ../../Source/Expression/*.cpp
+gcov -o ./Obj/ -l ../../Source/Analyzer/*.cpp
+gcov -o ./Obj/ -l ../../Source/Runtime/*.cpp
 mv *.gcov ./Coverage/
