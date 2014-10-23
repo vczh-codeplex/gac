4b0087eff968f7741552a90e87630943e2839f65
vczh_cp <SND\vczh_cp>
2014-10-23 14:41:34 +0000

    gcov

--- Common/Linux/coverage.sh
+++ Common/Linux/coverage.sh
@@ -1,3 +1,8 @@
 mkdir Coverage
-gcov -o ./Obj/ ../Source/*.cpp ../Source/Collection/*.cpp ../Source/Parsing/*.cpp ../Source/Reflection/*.cpp ../Source/Regex/*.cpp ../Source/Stream/*.cpp
+gcov -o ./Obj/ -l ../Source/*.cpp
+gcov -o ./Obj/ -l ../Source/Collection/*.cpp
+gcov -o ./Obj/ -l ../Source/Parsing/*.cpp
+gcov -o ./Obj/ -l ../Source/Reflection/*.cpp
+gcov -o ./Obj/ -l ../Source/Regex/*.cpp
+gcov -o ./Obj/ -l ../Source/Stream/*.cpp
 mv *.gcov ./Coverage/
