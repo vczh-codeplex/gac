921fb86c693705ce639717dc4d887ccd5f9d957e
vczh_cp <SND\vczh_cp>
2015-03-04 03:44:39 +0000

    Add VS-like dark skin to GacStudio

--- Libraries/GacUI/GacUISrc/GacStudio/UI/GacStudioUI.h
+++ Libraries/GacUI/GacUISrc/GacStudio/UI/GacStudioUI.h
@@ -11,6 +11,10 @@ DO NOT MODIFY
 #ifndef VCZH_GACUI_RESOURCE_CODE_GENERATOR_GacStudioUI
 #define VCZH_GACUI_RESOURCE_CODE_GENERATOR_GacStudioUI
 
+#include "GsHScrollTemplate.h"
+#include "GsHTrackerTemplate.h"
+#include "GsVScrollTemplate.h"
+#include "GsVTrackerTemplate.h"
 #include "GsAddExistingFilesWindow.h"
 #include "GsMainWindow.h"
 #include "GsNewFileWindow.h"
